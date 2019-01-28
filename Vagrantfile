ae85f726319841f53f6d1e1e35ce791d087d05df
Alexander Sheiko <alexander.sheiko@mgid.com>
2019-01-28 12:49:25 +0200

    Many improvements

--- Vagrantfile
+++ Vagrantfile
@@ -1,17 +1,22 @@
 static_ip = "172.16.0.8"
 
 Vagrant.configure("2") do |config|
-  config.vm.box = "ubuntu/xenial64"
+	config.vm.box = "ubuntu/xenial64"
+	config.vm.box_check_update = false
   config.vm.provider "virtualbox" do |v|
-	v.memory = 4096
-	v.cpus = 2
+		v.memory = 4096
+		v.cpus = 2
   end
   config.vm.define "k8s" do |k8s|
 	k8s.vm.hostname = "k8s"
 	k8s.vm.network "private_network", ip: static_ip
 	k8s.vm.provision "shell" do |shell|
 		shell.path = "provision.sh"
-		shell.env = {"STATIC_IP" => static_ip, "SKIP_INIT" => ENV['SKIP_INIT'] }
+		shell.env = {
+			"STATIC_IP" => static_ip,
+			"KUBE_VERSION" => ENV['KUBE_VERSION'],
+			"SKIP_INIT" => ENV['SKIP_INIT']
+		}
 	end
   end
 end
--- provision.sh
+++ provision.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
-set -euo pipefail
+set -eo pipefail
 
 echo '### Adding repositories ###'
 export DEBIAN_FRONTEND=noninteractive
-apt-get update && apt-get install -y apt-transport-https curl
+apt-get update -qq && apt-get install -yq apt-transport-https curl
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
 echo "deb https://download.docker.com/linux/$(lsb_release -si | tr '[:upper:]' '[:lower:]') $(lsb_release -cs) stable" > /etc/apt/sources.list.d/docker.list
 curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
 echo "deb http://apt.kubernetes.io/ kubernetes-xenial main" > /etc/apt/sources.list.d/kubernetes.list
 
 echo '### Install docker and kubernetes ###'
-apt-get update && apt-get install -y kubelet kubeadm kubectl docker-ce=$(apt-cache madison docker-ce | grep 17.03 | head -1 | awk '{print $3}')
+apt-get update -qq && apt-get install -yq kubelet=$KUBE_VERSION* kubeadm=$KUBE_VERSION* kubectl=$KUBE_VERSION* docker-ce=18.06*
 
 echo '### Disable swap ###'
 sed -i -e '/ swap / s/^\(.*\)$/#\1/g' /etc/fstab
@@ -22,7 +22,7 @@ if [[ "$SKIP_INIT" -eq 1 ]]; then
 fi
 
 echo '### Initializing the cluster ###'
-kubeadm init --apiserver-advertise-address=$STATIC_IP --pod-network-cidr=192.168.0.0/16
+kubeadm init --kubernetes-version=$KUBE_VERSION --apiserver-advertise-address=$STATIC_IP --pod-network-cidr=192.168.0.0/16
 export KUBECONFIG=/etc/kubernetes/admin.conf
 
 echo '### Setting up the user environment ###'
@@ -31,47 +31,55 @@ echo "export KUBECONFIG=/etc/kubernetes/admin.conf" > /etc/profile.d/kube.sh
 echo "source <(kubectl completion bash)" >> /etc/profile.d/kube.sh
 echo "alias docker='sudo /usr/bin/docker'" > /etc/profile.d/docker.sh
 
-echo '### Install Calico networking ###'
-kubectl apply -f https://docs.projectcalico.org/v3.1/getting-started/kubernetes/installation/hosted/rbac-kdd.yaml
-kubectl apply -f https://docs.projectcalico.org/v3.1/getting-started/kubernetes/installation/hosted/kubernetes-datastore/calico-networking/1.7/calico.yaml
-
 echo '### Untaint master ###'
 kubectl taint nodes --all node-role.kubernetes.io/master-
 
-echo '### Install nginx ingress proxy ###'
-kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/master/deploy/namespace.yaml
-kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/master/deploy/default-backend.yaml
-kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/master/deploy/configmap.yaml
-kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/master/deploy/tcp-services-configmap.yaml
-kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/master/deploy/udp-services-configmap.yaml
-kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/master/deploy/rbac.yaml
-kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/master/deploy/with-rbac.yaml
-cat <<EOF | kubectl create -f -
-apiVersion: v1
-kind: Service
-metadata:
-  name: ingress-nginx
-  namespace: ingress-nginx
-spec:
-  ports:
-  - name: http
-    port: 80
-    targetPort: 80
-  - name: https
-    port: 443
-    targetPort: 443
-  selector:
-    app: ingress-nginx
-  externalIPs:
-  - $STATIC_IP
-EOF
+if [[ "$SKIP_CNI" -eq 1 ]]; then
+  echo 'SKIP CNI network installing!'
+else
+  echo '### Install Calico networking ###'
+  kubectl apply -f https://docs.projectcalico.org/v3.5/getting-started/kubernetes/installation/hosted/kubernetes-datastore/calico-networking/1.7/calico.yaml
+fi
 
-echo '### Install Heapster for metrics ###'
-kubectl apply -f https://raw.githubusercontent.com/kubernetes/heapster/master/deploy/kube-config/rbac/heapster-rbac.yaml
-kubectl apply -f https://raw.githubusercontent.com/kubernetes/heapster/master/deploy/kube-config/standalone/heapster-controller.yaml
+if [[ "$SKIP_INGRESS" -eq 1 ]]; then
+  echo 'SKIP ingress installing!'
+else
+  echo '### Install nginx ingress proxy ###'
+  kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/master/deploy/mandatory.yaml
+cat <<EOF | kubectl create -f -
+  apiVersion: v1
+  kind: Service
+  metadata:
+    name: ingress-nginx
+    namespace: ingress-nginx
+    labels:
+      app.kubernetes.io/name: ingress-nginx
+      app.kubernetes.io/part-of: ingress-nginx
+  spec:
+    ports:
+      - name: http
+        port: 80
+        targetPort: 80
+        protocol: TCP
+      - name: https
+        port: 443
+        targetPort: 443
+        protocol: TCP
+    selector:
+      app.kubernetes.io/name: ingress-nginx
+      app.kubernetes.io/part-of: ingress-nginx
+    externalIPs:
+    - $STATIC_IP
+EOF
+fi
+
+if [[ "$SKIP_DASHBOARD" -eq 1 ]]; then
+  echo 'SKIP dashboard installing!'
+  exit;
+fi
 
 echo '### Install dashboard ###'
-kubectl apply -f https://raw.githubusercontent.com/kubernetes/dashboard/master/src/deploy/alternative/kubernetes-dashboard.yaml
+kubectl apply -f https://raw.githubusercontent.com/kubernetes/dashboard/v1.10.1/src/deploy/alternative/kubernetes-dashboard.yaml
 cat <<EOF | kubectl create -f -
 apiVersion: rbac.authorization.k8s.io/v1beta1
 kind: ClusterRoleBinding
@@ -105,5 +113,5 @@ spec:
   externalIPs:
   - $STATIC_IP
 EOF
-kubectl wait --timeout=60s --for=condition=available -n kube-system deployment/kubernetes-dashboard
+kubectl wait --timeout=300s --for=condition=available -n kube-system deployment/kubernetes-dashboard
 echo ">>> Dashboard on http://$STATIC_IP:9090 <<<"
